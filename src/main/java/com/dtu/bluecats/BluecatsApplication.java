433c405a3df503e63031eb1bc71916183ea2df7e
Lakshay Grover <lakshaygrover7@gmail.com>
2022-08-19 14:07:24 +0530

    first commit

---
+++ src/main/java/com/dtu/bluecats/BluecatsApplication.java
@@ -0,0 +1,13 @@
+package com.dtu.bluecats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluecatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluecatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dtu/bluecats/BluecatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dtu.bluecats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluecatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
